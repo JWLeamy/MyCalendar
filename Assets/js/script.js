4d6255a61d5a7377c0e5092bc99d36d9e8c725bd
John Leamy <jwleamy@gmail.com>
2022-11-29 15:31:09 -0800

    put all js function within jqeury call

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,29 +1,18 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-var todoList = $('.todolist')
 var textinput = $('#textinput')
 
 var today = dayjs();
+//make the live server update current date and time
 $('#currentDay').text(today.format('dddd, MMMM D, YYYY h:mm A'))
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 
 $(function () {
-  for (var i = 9; i < 18; i++) {
-    var thisidvalue = ('#hour-' + i.toString())
-    var balhh = $(thisidvalue).children('#textinput').html()
-
-    var texte = $(thisidvalue).children('#textinput').text()
-
-    if (localStorage.getItem(thisidvalue) !== null) {
-      var stored = JSON.parse(localStorage.getItem(thisidvalue))
   
-      balhh = $(thisidvalue).children('#textinput').html(stored)
-    }
-  }
-    var liveHourID = dayjs().format('H');
+  returnlocal()
+    
+  var liveHourID = dayjs().format('H');
 
-    var livehourInt = parseInt(liveHourID)
+  var livehourInt = parseInt(liveHourID)
 
     for (var i = 9; i < 18; i++) {
       if (i < livehourInt) {
@@ -31,84 +20,62 @@ $(function () {
         $("#hour-" + i.toString()).addClass('past')
       }
     }
-
-        function updateClass() {
-          //targets the live hour of the day
-          var liveHourID = "hour-" + dayjs().format('H');
-          // targets ID tag
-          var livePresentCard = $('#' + liveHourID);
-          //targets any card with classtag ".present"
-          var oldPresentCard = $('.present');
-          //retreives id of present card
-          var oldPresentCardID = $('.present').attr('id');
-          
-          var hourIsChanged = (oldPresentCardID !== liveHourID);
-          if (hourIsChanged) {
-            oldPresentCard.addClass('past');
-            oldPresentCard.removeClass('present');
-            livePresentCard.addClass('present');
-            livePresentCard.removeClass('future');
-          };
-        };
   
-  
-      setInterval(updateClass, 1000);
+  setInterval(updateClass, 1000);
 
+  document.addEventListener('click', function(event) {
+
+    var element = event.target;
+  
+    if (element.matches("button")) {
+      //retrieves ID name from selected element
+      var thisid = element.parentElement.getAttribute("Id");
+      //adds hashtag to use later
+      var idname = ('#' + thisid)
+      //retrieves the value of the inputed text by the user (within a timeslot)
+      var balhh = $(idname).children('#textinput').val()
+      //sets local storage of that tieslot to whatever text the user added
+      localStorage.setItem((idname), JSON.stringify(balhh))
+    }
+  })
       
 });
 
-document.addEventListener('click', function(event) {
 
-  var element = event.target;
-
-  if (element.matches("button")) {
-    //retrieves ID name from selected element
-    var thisid = element.parentElement.getAttribute("Id");
-    //adds hashtag to use later
-    var idname = ('#' + thisid)
-    //retrieves the value of the inputed text by the user (within a timeslot)
-    var balhh = $(idname).children('#textinput').val()
-    //sets local storage of that tieslot to whatever text the user added
-    localStorage.setItem((idname), JSON.stringify(balhh))
-  }
-})
-
-
-/*document.addEventListener('click', function(event) {
-  var element = event.target;
-
-  // TODO: Describe the functionality of the following `if` statement.
-  if (element.matches("button")) {
-    var thisid = element.parentElement.getAttribute("Id");
-    console.log(thisid)
-    var ye = element.parentElement
-    var ye2 = ye.children(textinput)
-    console.log(ye2)
-    var thistext = thisid.children('.textinput')
-    console.log(thistext)
-    localStorage.setItem(thisid, thisid[1])
+//function to render previous input from local storage
+function returnlocal () {
+    for (var i = 9; i < 18; i++) {
     
-  }
-});
-*/
-// if time (after parse) is less than live time, add area of past.
+      var thisidvalue = ('#hour-' + i.toString())
 
+      var balhh = $(thisidvalue).children('#textinput').html()
 
-/*function Comp() {
-  var liveHourID = "hour-" + dayjs().format('H');
-  //targets any card with classtag ".present"
-  var FutureCard = $('.future');
-  //retreives id of present card
-  var FutureCardId = $('.future').attr('id');
-  
-  var ishour = (FutureCardId === liveHourID);
-  
-  if (ishour) {
-    FutureCard.addClass('present')
-    FutureCard.removeClass('future')
+        if (localStorage.getItem(thisidvalue) !== null) {
+          
+          var stored = JSON.parse(localStorage.getItem(thisidvalue))
+
+          $(thisidvalue).children('#textinput').html(stored)
+        }
   }
 }
-*/
 
-
+//function to update class coloring based off time
+function updateClass() {
+  //targets the live hour of the day
+  var liveHourID = "hour-" + dayjs().format('H');
+  // targets ID tag
+  var livePresentCard = $('#' + liveHourID);
+  //targets any card with classtag ".present"
+  var oldPresentCard = $('.present');
+  //retreives id of present card
+  var oldPresentCardID = $('.present').attr('id');
+  
+  var hourIsChanged = (oldPresentCardID !== liveHourID);
+  if (hourIsChanged) {
+    oldPresentCard.addClass('past');
+    oldPresentCard.removeClass('present');
+    livePresentCard.addClass('present');
+    livePresentCard.removeClass('future');
+  };
+};
 
